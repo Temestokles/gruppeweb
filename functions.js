4734e978e716fb1f2b366f4af66e79ecf428733e
Saja <sajathequeen@hotmail.com>
2013-11-07 10:51:06 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,102 @@
+//Toogle funksjoner for siden uteliv.php
+
+$( document ).ready(function(){
+$('#merinfoHorgans').hide(0);
+
+$('#lesmerHorgans').click(function(){
+$('#merinfoHorgans').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoVu').hide(0);
+
+$('#lesmerVu').click(function(){
+$('#merinfoVu').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoVulkan').hide(0);
+
+$('#lesmerVulkan').click(function(){
+$('#merinfoVulkan').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoNox').hide(0);
+
+$('#lesmerNox').click(function(){
+$('#merinfoNox').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoNivou').hide(0);
+
+$('#lesmerNivou').click(function(){
+$('#merinfoNivou').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoSo').hide(0);
+
+$('#lesmerSo').click(function(){
+$('#merinfoSo').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoJæger').hide(0);
+
+$('#lesmerJæger').click(function(){
+$('#merinfoJæger').toggle(0);
+} ) ;
+}
+	)
+;
+
+
+$( document ).ready(function(){
+$('#merinfoVilla').hide(0);
+
+$('#lesmerVilla').click(function(){
+$('#merinfoVilla').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoBlå').hide(0);
+
+$('#lesmerBlå').click(function(){
+$('#merinfoBlå').toggle(0);
+} ) ;
+}
+	)
+;
+
+$( document ).ready(function(){
+$('#merinfoRå').hide(0);
+
+$('#lesmerRå').click(function(){
+$('#merinfoRå').toggle(0);
+} ) ;
+}
+	)
+;
