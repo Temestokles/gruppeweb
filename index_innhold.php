ba3b71a4828c525083766ea228d4e1631b68f1b6
Saja <sajathequeen@hotmail.com>
2013-11-07 10:51:35 +0100

    Create index_innhold.php

---
+++ index_innhold.php
@@ -0,0 +1,12 @@
+<section class="indexInnhold">
+				<article>
+					<h2> Velkommen til Oslo guiden </h2>
+						<p>guhhlpkjnigytfrgyhujikmojiuftygu</p>
+						<p>rdtfgyhujikomjhug</p>
+						<p>yfghujikl,øæ.jasdjAGSDJAHSGDJHASDJHASDJHasdva</p>
+						<p>jkcskdjvgjsdvgjsdbcvjhsdvhjsd</p>
+						<p>jkscahkjSDGChsdjBCsjkd</p>
+				</article>
+
+			<img src="prosjekt.jpg" alt="bilde">
+</section>
