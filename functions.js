a6896101ae7b31f46c244569a7ee0611223d1555
Saja <sajathequeen@hotmail.com>
2013-11-10 18:09:45 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,102 +1,376 @@
-//Toogle funksjoner for siden uteliv.php
 
-$( document ).ready(function(){
-$('#merinfoHorgans').hide(0);
+/* FUNKSJONER TIL UTELIV */
 
-$('#lesmerHorgans').click(function(){
-$('#merinfoHorgans').toggle(0);
-} ) ;
+
+/* FUNSKJONER TIL HORGANS */ 
+
+$(document).ready(function(){
+$("#lesmerHorgans").click(function(){
+$("#merinfoHorgans").fadeIn("slow");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoVu').hide(0);
+});
 
-$('#lesmerVu').click(function(){
-$('#merinfoVu').toggle(0);
-} ) ;
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoHorgans").fadeOut("fast");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoVulkan').hide(0);
+});
 
-$('#lesmerVulkan').click(function(){
-$('#merinfoVulkan').toggle(0);
-} ) ;
+$(document).ready(function(){
+$("#commentsHorgans").click(function(){
+$("#comment_boks_Horgans").fadeIn("slow");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoNox').hide(0);
+});
 
-$('#lesmerNox').click(function(){
-$('#merinfoNox').toggle(0);
-} ) ;
+$(document).ready(function(){
+$("knapp1").click(function(){
+$("#comment_boks_Horgans").fadeOut("fast");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoNivou').hide(0);
+});
 
-$('#lesmerNivou').click(function(){
-$('#merinfoNivou').toggle(0);
-} ) ;
+/* FUNKSJONER TIL BAR VULKAN*/
+
+
+$(document).ready(function(){
+$("#lesmerVulkan").click(function(){
+$("#merinfoVulkan").fadeIn("slow");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoSo').hide(0);
+});
 
-$('#lesmerSo').click(function(){
-$('#merinfoSo').toggle(0);
-} ) ;
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoVulkan").fadeOut("fast");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoJæger').hide(0);
+});
 
-$('#lesmerJæger').click(function(){
-$('#merinfoJæger').toggle(0);
-} ) ;
+$(document).ready(function(){
+$("#commentsVulkan").click(function(){
+$("#comment_boks_Vulkan").fadeIn("slow");
 }
-	)
-;
+);
 
+});
 
-$( document ).ready(function(){
-$('#merinfoVilla').hide(0);
-
-$('#lesmerVilla').click(function(){
-$('#merinfoVilla').toggle(0);
-} ) ;
+$(document).ready(function(){
+$("knapp2").click(function(){
+$("#comment_boks_Vulkan").fadeOut("fast");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoBlå').hide(0);
+});
 
-$('#lesmerBlå').click(function(){
-$('#merinfoBlå').toggle(0);
-} ) ;
+/* FUNKSJONER TIL NIVOU */
+
+$(document).ready(function(){
+$("#lesmerNivou").click(function(){
+$("#merinfoNivou").fadeIn("slow");
 }
-	)
-;
+);
 
-$( document ).ready(function(){
-$('#merinfoRå').hide(0);
+});
 
-$('#lesmerRå').click(function(){
-$('#merinfoRå').toggle(0);
-} ) ;
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoNivou").fadeOut("fast");
 }
-	)
-;
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsNivou").click(function(){
+$("#comment_boks_Nivou").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp3").click(function(){
+$("#comment_boks_Nivou").fadeOut("fast");
+}
+);
+
+});
+
+
+/* FUNKSJONER TIL JÆGER */
+
+$(document).ready(function(){
+$("#lesmerJaeger").click(function(){
+$("#merinfoJaeger").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoJaeger").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsJaeger").click(function(){
+$("#comment_boks_Jaeger").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp4").click(function(){
+$("#comment_boks_Jaeger").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL BLÅ */
+
+
+$(document).ready(function(){
+$("#lesmerBlaa").click(function(){
+$("#merinfoBlaa").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoBlaa").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsBlaa").click(function(){
+$("#comment_boks_Blaa").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp5").click(function(){
+$("#comment_boks_Blaa").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL VU */
+
+
+$(document).ready(function(){
+$("#lesmerVu").click(function(){
+$("#merinfoVu").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoVu").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsVu").click(function(){
+$("#comment_boks_Vu").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp6").click(function(){
+$("#comment_boks_Vu").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL NOX */
+
+
+
+$(document).ready(function(){
+$("#lesmerNox").click(function(){
+$("#merinfoNox").fadeIn("slow");
+}
+);
+
+});
+
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoNox").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsNox").click(function(){
+$("#comment_boks_Nox").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp7").click(function(){
+$("#comment_boks_Nox").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL SO */
+
+
+$(document).ready(function(){
+$("#lesmerSo").click(function(){
+$("#merinfoSo").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoSo").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsSo").click(function(){
+$("#comment_boks_So").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp8").click(function(){
+$("#comment_boks_So").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL VILLA */
+
+
+$(document).ready(function(){
+$("#lesmerVilla").click(function(){
+$("#merinfoVilla").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoVilla").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsVilla").click(function(){
+$("#comment_boks_Villa").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp9").click(function(){
+$("#comment_boks_Villa").fadeOut("fast");
+}
+);
+
+});
+
+/* FUNKSJONER TIL RÅ */
+
+$(document).ready(function(){
+$("#lesmerRaa").click(function(){
+$("#merinfoRaa").fadeIn("slow");
+}
+);
+
+});
+
+
+$(document).ready(function(){
+$(".close_button").click(function(){
+$("#merinfoRaa").fadeOut("fast");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("#commentsRaa").click(function(){
+$("#comment_boks_Raa").fadeIn("slow");
+}
+);
+
+});
+
+$(document).ready(function(){
+$("knapp10").click(function(){
+$("#comment_boks_Raa").fadeOut("fast");
+}
+);
+
+});
+
+/* SLUTT PÅ FUNKSJONER UTELIV */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
