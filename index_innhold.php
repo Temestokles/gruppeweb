bc33c6f14da9e423d6a5e50c67a5a6f094ce6926
Saja <sajathequeen@hotmail.com>
2013-11-13 21:57:16 +0100

    lagt til forsidebild og litt tekst i index

--- index_innhold.php
+++ index_innhold.php
@@ -1,12 +1,17 @@
 <section class="indexInnhold">
-				<article>
-					<h2> Velkommen til Oslo guiden </h2>
-						<p>guhhlpkjnigytfrgyhujikmojiuftygu</p>
-						<p>rdtfgyhujikomjhug</p>
-						<p>yfghujikl,øæ.jasdjAGSDJAHSGDJHASDJHASDJHasdva</p>
-						<p>jkcskdjvgjsdvgjsdbcvjhsdvhjsd</p>
-						<p>jkscahkjSDGChsdjBCsjkd</p>
+				
+					
+				<aside>
+					<article>
+						<h2> Oslo "Tigerstaden" </h2>
+					<p> Tigerstaden er et kallenavn for Oslo som opprinnelig er brukt for å beskrive Oslo som en farlig og ubarmhjertig storby.
+					Denne symbol for byen ble nevnt i en veldig kjent dikt på 1800-tallet og ble kalt for "denne kolde by"<br>
+					Begrepet brukes i dag som oftest av folk som bor utenbys uten nødvendigvis med den negative referansen det opprinnelig hadde.<br>
+					For mange folk i Oslo selv er begrepet positivt ladet og brukes med stolthet.<br>
+					Kilde: Wikipedia</p>	
 				</article>
-
-			<img src="prosjekt.jpg" alt="bilde">
+			</aside>
+			<figure>
+			<img src="BilderWebb/Tiger1.jpg" alt="bilde av Tiger på Oslo S" width="60%">
+			</figure>
 </section>
