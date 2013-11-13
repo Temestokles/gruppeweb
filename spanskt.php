99a732e158ed66d7b51a961adb4271f7da8b71cf
Saja <sajathequeen@hotmail.com>
2013-11-13 18:38:27 +0100

    ny bild bl.a

---
+++ spanskt.php
@@ -0,0 +1,32 @@
+<section class="spanskt main">
+
+
+
+<table>
+
+<tr>
+	<td> 
+			<img src="BilderWebb/Tapas.JPG" Alt="Bild fra Dattra til Hagen" width="150">
+	</td>
+
+	<td>	<p><strong>DATTRA TIL HAGEN </strong><br>
+			Dattra til hagen er en fargerik bar, cafè og klubb fordelt over to etasjer og Oslos fineste og koseligeste bakgård.<br>
+			Dattra byr pået godt utvalg av både varm og kald drikke og maten varier mellom lunsjretter og nydelige tapasretter, <br>
+			til a la cartè. Her er det humane priser på både drykk og mat.<br>
+			Adresse: Grønland 10, 0188 Oslo <br>
+			Source:<a href="http://www.dattra.no">www.dattra.no</a></p>
+	</td>
+
+<tr>
+
+	<td>
+			<img src="BilderWebb/delikatessen.jpg" Alt="Bild fra Delikatessen" width="150">
+	</td>
+
+	<td>	<p><strong>DELIKATESSEN</strong><br>
+			Delikatessen er kjent for sin gode tapas og det finns idag tre restauranter i Oslo, Aker Brygge, Majorstuen og Grünerløkka.<br>
+			Prismessigt ligger de på litt over middels men søker du etter kvalitè og ekte spansk tapas så har du kommit till rett stede.<br>
+			Adresse:Aker Brygge, Holmens gate 2<br>
+			Source:<a href="http://www.delicatessen.no"> www.delicatessen.no</a></p>
+
+</section>
