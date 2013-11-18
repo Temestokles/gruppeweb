7282fed72d0a78107f06d49d96830387a904b0d8
Vegard Vollebekk <volveg12@nith.no>
2013-11-18 10:07:06 +0100

    La til beta av kultursiden med medfølegende bilder

---
+++ kulturaktiviteter.php
@@ -0,0 +1,214 @@
+
+<tr>
+	<td>
+		<img src="./BilderWebb/kulturaktiviteter/kulturhus.jpg" Alt="" width="150">
+	</td>
+	<td>
+		<p>
+			<strong>Kultureiendommer</strong>
+			<br />
+			Kulturetaten forvalter kulturbygg, kulturminner og historiske gårdsanlegg. Etaten fremskaffer lokaler til kunst- og kulturformål i Oslo og tilrettelegger for fornuftig bruk av historiske gårdsanlegg.
+			<br />
+		</p>
+		<div id="Kultureiendommer">
+			<div class="close_button"> X </div>
+			<img src="./BilderWebb/kulturaktiviteter/kulturhus.jpg" Alt="" width="250">
+			<p>
+
+				Kulturbygg og historiske gårdsanlegg leies ut på subsidierte vilkår, og inneholder kunst- og kulturaktiviteter av byomfattende karakter. Kulturetaten har 40 kultureiendommer med 64 bygninger. I tillegg kommer forvaltningsansvar for Middelalderbeltet på totalt på 90 743 m2 og feriekolonien på Hudøy med sine 64 ulike bygninger. Kulturetaten har ansvaret for drift, eiendomsutvikling, utleie, vedlikehold og rehabilitering av disse kultureiendommene. Det er også mulig å leie lokaler for kortere tidsrom, eksempelvis til foredrag, seminarer, bryllup og lignende.
+
+				Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+			</div>
+		</td>
+	</tr>
+	
+	<tr> 
+		<td>
+			<ul>
+				<li class="lesMer" data-target="merinfoKulturaktiviteter">Les mer..</li>
+			</ul>			
+		</td>
+	</tr>
+
+
+	<tr>
+		<td>
+			<img src="./BilderWebb/kulturaktiviteter/kulturkortet.jpg" Alt="" width="150">
+		</td>
+		<td>
+			<p>
+				<strong>Kulturkort for ungdom</strong>
+				<br />
+				Kortet gir unge mellom 16 og 20 år i Oslo tilgang på billetter med 20-50% rabatt på kunst og kulturopplevelser ved 26 institusjoner. Formålet er at unge skal benytte seg av kulturtilbudene oftere, og at nye publikummere skal rekrutteres. Kulturkortet er gratis, og gyldig sammen med aldersbevis fra du har fylt 16 til den dagen du fyller 20.
+				<br />
+			</p>
+			<div id="Kulturkort">
+				<div class="close_button"> X </div>
+				<img src="./BilderWebb/kulturaktiviteter/kulturkortet.jpg" Alt="" width="250">
+				<p>
+
+					All ungdom i målgruppen vil i løpet av en treårig prøveperiode få tildelt kulturkort.
+
+					For alle
+					Ordningen er ikke forbeholdt elever i videregående skole, men skal også gi tilbud til de rundt 7000 unge som ikke er i skolesystemet. Oslo har totalt 23 000 unge i målgruppen.
+
+					Hvordan få tak i kort?
+					Elever i videregående skole får kortet utdelt på skolen. Andre kan hente sitt kort ved samtlige av Deichmanske biblioteks filialer, på Popsentret på Schaus plass eller ved UngInfo i Møllergata 3.
+
+					Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+				</div>
+			</td>
+		</tr>
+
+		<tr> 
+			<td>
+				<ul>
+					<li class="lesMer" data-target="merinfoKulturaktiviteter">Les mer..</li>
+				</ul>			
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				<img src="./BilderWebb/kulturaktiviteter/kunstIOslo.jpg" Alt="" width="150">
+			</td>
+			<td>
+				<p>
+					<strong>Kunst i Oslo</strong>
+					<br />
+					Kunst i Oslo har ansvaret for all kunst i Oslo kommunes eie, utenom Munch-, Stenersen- og Vigeland-museet.
+					Samlingen omfatter 18 000 kunstverk. Denne samlingen finnes ikke i et galleri eller museum, men rundt om på kommunale bygg og institusjoner, i offentlige rom og på kontorer.
+					<br />
+				</p>
+				<div id="Kunst">
+					<div class="close_button"> X </div>
+					<img src="./BilderWebb/kulturaktiviteter/kunstIOslo.jpg" Alt="" width="250">
+					<p>
+
+						Kunst i Oslo forvalter også Oslo kommunes kunstordning.
+						Denne ordningen gjør at det avsettes 0,5 prosent av av kommunens totalte investeringsbudsjett til kunstprosjekter.
+						Regelverket gjelder alle Oslo kommunes virksomheter og anvendes ved for eksempel kommunale nybygg, rehabilitering, idrettsanlegg, trafikkanlegg, park- og byrom.
+
+						Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+					</div>
+				</td>
+			</tr>
+
+			<tr> 
+				<td>
+					<ul>
+						<li class="lesMer" data-target="merinfoKulturaktiviteter">Les mer..</li>
+					</ul>			
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<img src="./BilderWebb/kulturaktiviteter/deichman.jpg" Alt="" width="150">
+				</td>
+				<td>
+					<p>
+						<strong>Deichmanske bibliotek</strong>
+						<br />
+						Deichmanske bibliotek er Norges største og eldste folkebibliotek, og består av 16 filialer over hele byen, flere spesialavdelinger og et hovedbibliotek i sentrum.  Vi er en viktig møteplass i nærmiljøet og en god støttespiller for frivillige organisasjoner og andre initiativtakere.
+						<br />
+					</p>
+					<div id="Deichmanske">
+						<div class="close_button"> X </div>
+						<img src="./BilderWebb/kulturaktiviteter/deichman.jpg" Alt="" width="250">
+						Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+					</div>
+				</td>
+			</tr>
+
+			<tr> 
+				<td>
+					<ul>
+						<li class="lesMer" data-target="merinfoKulturaktiviteter">Les mer..</li>
+					</ul>			
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<img src="./BilderWebb/kulturaktiviteter/kulturnatt.jpg" Alt="" width="150">
+				</td>
+				<td>
+					<p>
+						<strong>Kulturnatt i Oslo</strong>
+						<br />
+						Én kveld i året er det mulig å teste ut det fantastiske kulturlivet i Oslo - helt gratis.
+						Oslo kulturnatt gir en prøvesmak av det kulturelle mangfoldet, bredden og kvaliteten i Oslos kulturliv.
+						<br />
+					</p>
+					<div id="Kulturnatt">
+						<div class="close_button"> X </div>
+						<img src="./BilderWebb/kulturaktiviteter/kulturnatt.jpg" Alt="" width="250">
+						Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+					</div>
+				</td>
+			</tr>
+
+
+			<tr>
+				<td>
+					<img src="./BilderWebb/kulturaktiviteter/popsenterfasade.jpg" Alt="" width="150">
+				</td>
+				<td>
+					<p>
+						<strong>Popsenteret</strong>
+						<br />
+						Én kveld i året er det mulig å teste ut det fantastiske kulturlivet i Oslo - helt gratis.
+						Oslo kulturnatt gir en prøvesmak av det kulturelle mangfoldet, bredden og kvaliteten i Oslos kulturliv.
+						<br />
+					</p>
+					<div id="Popsenteret">
+						<div class="close_button"> X </div>
+						<img src="./BilderWebb/kulturaktiviteter/popsenterfasade.jpg" Alt="" width="250">
+						Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+					</div>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<img src="./BilderWebb/kulturaktiviteter/vigeland.jpg" Alt="" width="150">
+				</td>
+				<td>
+					<p>
+						<strong>Vigeland-museet</strong>
+						<br />
+						Vigeland-museet er Oslos skulpturmuseum. Museet er viet til Gustav Vigeland. I tillegg er museet det viktigste visningsstedet for tredimensjonal kunst i Oslo.
+						<br />
+					</p>
+					<div id="Vigeland">
+						<div class="close_button"> X </div>
+						<img src="./BilderWebb/kulturaktiviteter/vigeland.jpg" Alt="" width="250">
+						Source:<a href="kulturetaten.oslo.kommune.no" target="blank">kulturetaten.oslo.kommune.no</a></p>
+					</div>
+				</td>
+			</tr>
+
+
+
+	<tr>
+		<td>
+			<img src="./BilderWebb/kulturaktiviteter/kulturkortet.jpg" Alt="" width="150">
+		</td>
+		<td>
+			<p>
+				<strong>Norske Operaen</strong>
+				<br />
+				Den Norske Opera og Ballett er den eneste fullt ut profesjonelle institusjon for produksjon og formidling av opera og ballett i Norge, og ble stiftet i 1957. Institusjonen er organisert som et aksjeselskap og var til 2008 eid av staten (90%) og Det Norske Operafond (10%). Den er nå heleid av den norske stat.
+				<br />
+			</p>
+			<div id="Kulturkort">
+				<div class="close_button"> X </div>
+				<img src="./BilderWebb/kulturaktiviteter/kulturkortet.jpg" Alt="" width="250">
+				<p>
+					Operaens første sjef var Kirsten Flagstad. Den norske Opera og Ballett holdt siden starten til i Folketeatret på Youngstorget, før den i 2008 kunne flytte inn i det nye Operahuset i Oslo med adresse Kirsten Flagstads plass 1 i Bjørvika. I forbindelse med flyttingen endret institusjonen navn fra «Den Norske Opera» til «Den Norske Opera og Ballett». Samtidig la Det norske Operafond ned virksomheten og overlot egenkapitalen til institusjonen.
+					Hjemmeside for mer informasjon om pågående forestillinger: <a href="http://www.operaen.no">http://www.operaen.no</a>
+					Source:<a href="http://no.wikipedia.org/wiki/Den_Norske_Opera_%26_Ballett" target="blank">http://no.wikipedia.org/wiki/Den_Norske_Opera_%26_Ballett</a></p>
+				</div>
+			</td>
+		</tr>
