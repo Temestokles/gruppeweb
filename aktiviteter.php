f1d883559c1a9fc068145d05927e5921627d4020
Temestokles <jonas-m@hotmail.de>
2013-11-19 13:29:07 +0100

    added 2 aktivities

--- aktiviteter.php
+++ aktiviteter.php
@@ -2,10 +2,35 @@
 <h2>AKTIVITETER</h2>
 
 <table>
-fotball
-fridrett
+
 bading
-kino
+
+<tr>
+	<td>
+		<img src="solli.png" alt="bilde" width="200">
+	</td>
+	<td>
+		<p><strong>Solli Bowling</strong><br>
+		Solli Bowling er Oslos eldste bowlinghall, og ligger sentralt i Oslo sentrum.
+		Hallen rommer 11 baner hvor man enten kommer på drop-in eller man kan sikre seg plass ved å bestille i forveien.
+		I tillegg til bowling tilbyr vi også biljard og airhocey. I etasjen over bowlinghallen befinner Peppes Pizza seg,
+		og klassikeren på Solli har i en årrekke vært å kombinere Pizza og Bowling for på den måten å få en helaften på by`n.
+	</td>
+</tr>
+
+
+<tr>
+	<td>
+		<img src="buddha.png" alt="bilde" width="200">
+	</td>
+	<td>
+		<p><strong>Buddha Biljard</strong><br>
+		Buddha Biljard er Oslo’s feteste biljardlounge og sportsbar med 13 biljardbord, storskjerm, gratis dart og
+		trådløs internett fordelt på 700 stemningsfulle kvadratmeter i Oslo sentrum. Ta med vennene dine og nyt kvelden
+		i en behagelig atmosfære med god musikk.
+	</td>
+</tr>
+
 
 	<tr>
 		<td>
@@ -15,12 +40,12 @@ kino
 			<p><strong>Oslo Judo Klubb</strong><br />
 				..ble opprettet i 1984. <br />
 				Vi var den gang 17 medlemmer. Til  begynne med hadde vi ikke egen dojo. De frste tre mnedene trente vi p Kristelig Gymnas p lnte brytematter. Vi var en ivrig treningsvillig gjeng som hadde mye moro. Deretter fikk vi lne en lite rom med matter i Ekeberghallen, fr vi fikk leie dojo i kjelleren p Skullerudstua. Der holdt vi til fram til 2007 fr vi endelig fikk egne lokaler i gamle gymsal p Bler skole.
-			<br /> 
+			<br />
 			</p>
 			<div id="merinfoAktiviteter">
 				<div class="close_button"> X </div>
-				<img src="BilderWebb/aktiviteter/judo_clipart.jpg" alt="Bilde av noen som gjennomfører et judokast" width="250">
-				
+				<img src="BilderWebb/aktiviteter/judo_clipart.jpg" alt="Bilde av noen som gjennomfører et judokast" width="200">
+
 				<p>Klubben har i dag ca. 200 medlemmer og er en av de største judoklubbene i Norge.
 					Klubben er tilsluttet Norges Judoforbund og Norges Idrettsforbund.
 					Gjennom rene har OJK trent frem flere norgesmestere bde individuelt og lag.
@@ -31,7 +56,7 @@ kino
 					<br />
 					Anbefalt alder for  starte med judo er 7-8 r.<br />
 					Source: <a href="http://www.oslojudoklubb.no/">www.oslojudoklubb.no</a>
-				</p>			
+				</p>
 			</div>
 		</td>
 	</tr>
@@ -43,12 +68,12 @@ kino
 		<td>
 			<p><strong>Oslo Karateklubb</strong><br />
 				Oslo Karateklubb er Norges eldste Karate og Nanbudoklubb, grunnlagt i 1969 av Rune Hagen. Oslo Karateklubb er medlem av Norges idrettsforbund.
-			<br /> 
+			<br />
 			</p>
 			<div id="merinfoAktiviteter">
 				<div class="close_button"> X </div>
 				<img src="BilderWebb/aktiviteter/shotok3.gif" alt="Bilde av noen som gjennomfører et karatespark mot hodet" width="250">
-				
+
 					<p>Hos oss kan du trene Nanbudo og Karate med erfarne instruktrer i et spennende og positivt milj, i  nye og innbydende lokaler. Vi har et tilbud til alle fra 7 r og oppover, om mlet er mosjon og helse eller toppidrett.
 
 					<br />Respekt for det vi trener og menneskene vi omgås, både i og utenfor treningslokalet, er utgangspunkt for all trening i Oslo Karateklubb.
@@ -56,7 +81,7 @@ kino
 					<br />Kampsport dreier seg ikke bare om  trene en idrett, men i stor grad om  f kontroll over bde kropp og sinn. Hos oss kan du lre deg noe som er bde nyttig og morsomt  og viktig.
 					<br />
 					Source: <a href="Source: http://www.oslokarateklubb.no/">www.oslokarateklubb.no</a>
-				</p>			
+				</p>
 			</div>
 		</td>
 	</tr>
@@ -68,12 +93,12 @@ kino
 		<td>
 			<p><strong>KLATREVERKET OG MYRENS SPORTSSENTER</strong><br />
 				Klatreverket er Nordens strste og mestomfattende klatresenter, beliggede i Oslo sentrum. Her er det et tilbud til klatrere p alle niver, fra den som aldri fr har klatret til den som konkurrerer i verdensmesterskap. Veggene vre er opp til 14,6 meter hye, men det er ogs lavere vegger som er greiere  klatre p i begynnelsen. Vinkelen p veggene er fra sva (hellende utover) til meget bratt (overhengende) og teksturen er alt fra finrplater med friksjonsoverflate, til stpte vegger med fjell-lignende overflate. Fordelt over tre niver i hovedrommet, er det tauklatring, rappelltrn og omfattende buldrearealer.
-			<br /> 
+			<br />
 			</p>
 			<div id="merinfoAktiviteter">
 				<div class="close_button"> X </div>
 				<img src="BilderWebb/aktiviteter/691904_klatring.bredde.web.jpg" alt="Bilde av noen som klatrer" width="250">
-				
+
 					Sammen med Golfsenteret, Treningssenteret og Oslo Squash utgjr vi Myrens Sportssenter. Vi har felles inngang og felles garderobeanlegg. Om du er fast kunde hos oss fr du rabatt p de andre aktivitetene p huset. Etter endt sportslig aktivitet kan det friste med mat og drikke p Myrens Kjkken, med samme inngang som oss.
 
 					<br />Vi har et bredt kurs- og aktivitetstilbud til nybegynnere og viderekomne barn, ungdom og voksne. Kurs holdes bde innendrs og utendrs, og vi har opplegg for barnegrupper, bursdagsfester, teambuilding, sosiale events eller spesialtilpassede opplegg.
@@ -83,7 +108,7 @@ kino
 					<br />Klatreverket pnet i november 2006, og ble raskt landets mest beskte klatresenter. Ideen om Klatreverket kom da Vegg i Vegg og Villmarkshuset p Grnland gikk mot sin alderdom. Eierne er ivrige klatrere med erfaring blant annet derfra, i tillegg til lang fartstid med sportsklatring og fjellklatring generelt. Vi ville lage landets desidert beste klatresenter, og Klatreverket ble resultatet.
 					<br />
 					Source: <a href="http://klatreverket.no/">www.klatreverket.no</a>
-				</p>			
+				</p>
 			</div>
 		</td>
 	</tr>
@@ -96,7 +121,7 @@ kino
 		<td>
 			<p><strong>OSLO VINTERPARK - TRYVANN</strong><br />
 				Tryvann Oslo Vinterpark ligger p toppen av Holmenkollen, bare 20 minutter fra Oslo sentrum. Her finnes 14 nedfarter og 7 heiser, hvorav 1 6-seters stolheis, og en 4-seters stolheise, lengste bakke er 1400 meter lang og anlegget har en fallhyde p 381 m. Tryvann har ogs eget omrde for barn og nybegynnere, stor terrengpark med halfpipe av internasjonal standard, utfordrende bakker, x-crosslype og cruisingbakker. Et kraftig snkanon-anlegg og 531 moh gjr at vi har normalsesong fra desember til medio april. Alle bakker er belyst for kveldskjring.
-			<br /> 
+			<br />
 			</p>
 			<div id="merinfoAktiviteter">
 				<div class="close_button"> X </div>
@@ -105,7 +130,7 @@ kino
 
 
 					Source: <a href="http://www.oslovinterpark.no/">www.oslovinterpark.no</a>
-				</p>			
+				</p>
 			</div>
 		</td>
 	</tr>
@@ -119,7 +144,7 @@ kino
 		<td>
 			<p><strong>Bitraf</strong><br />
 				Bitraf er et hackerspace / makerspace i Oslo som er pent for hvem som helst 7 dager i uka. Kom innom om du er interessert i programmering, digital kunst, elektronikk, fresing og fabrikasjon, fri programvare, spillutvikling eller andre liknende aktiviteter.
-			<br /> 
+			<br />
 			</p>
 			<div id="merinfoAktiviteter">
 				<div class="close_button"> X </div>
@@ -131,7 +156,7 @@ kino
 
 					<br />
 					<br />Source: <a href="https://bitraf.no/">https://bitraf.no</a>
-				</p>			
+				</p>
 			</div>
 		</td>
 	</tr>
