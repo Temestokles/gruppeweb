afd139eb87ea9733a6bd402b646f7bda28b87673
Vegard Vollebekk <volveg12@nith.no>
2013-11-19 10:43:57 +0100

    Fikset bildekilder

--- restauranter.php
+++ restauranter.php
@@ -85,7 +85,7 @@
 <h2> SPANSKT</h2>
 <tr>
 			 
-	<td> 	<img src="BilderWebb/Tapas.jpg" Alt="Bild på tapas fra Dattra" width="150"  height="90">
+	<td> 	<img src="BilderWebb/Tapas.JPG" Alt="Bild på tapas fra Dattra" width="150"  height="90">
 	</td>
 
 	<td>	<p><strong>DATTRA TIL HAGEN</strong><br>
@@ -96,7 +96,7 @@
 			<div id="merinfoDattra">
 				<div class="close_button"> X </div>
 
-				<img src="BilderWebb/Tapas.jpg" Alt="Bild på tapas fra Dattra" width="200">
+				<img src="BilderWebb/Tapas.JPG" Alt="Bild på tapas fra Dattra" width="200">
 				<p>Her er det humane priser på både drykk og mat.<br>
 				Adresse: Grønland 10, 0188 Oslo<br>
 				Source: <a href="http://www.dattra.no" target="blank">www.dattra.no</a></p>
