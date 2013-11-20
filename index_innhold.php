da31fa976a50f9fe09a12b5fd223cff06fda4530
Sofie Os <sofie_ao@hotmail.com>
2013-11-20 15:14:20 +0100

    Bildegreier på forsiden

--- index_innhold.php
+++ index_innhold.php
@@ -25,15 +25,7 @@
 			</aside>
 
 			<figure id="OsloKart">
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<img src="BilderWebb/OsloKarta.png" Alt="Bild på Oslo karta" width="400">
-=======
-				<img src="BilderWebb/OsloKarta.png" Alt="Bild på Oslo karta" width="450">
->>>>>>> 30e456469c845ff23a1960e5ee5741982992797d
-=======
-				<img src="BilderWebb/OsloKarta.png" Alt="Bild på Oslo karta" width="450">
->>>>>>> 5428fec2eae8d678965eab4df733033578fd8bae
+				<img src="BilderWebb/OsloKarta.jpg" Alt="Bild på Oslo karta" width="400">
 			</figure>
 
 		</div>
